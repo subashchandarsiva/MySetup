092ac7a4b708ddff83eeb2a1b954b8a915e6939f
subashchandarsiva <subashchandarsiva@gmail.com>
2020-09-03 21:23:48 +0100

    monhub

---
+++ monster-hub/build/precache-manifest.bf5e69844602a8fdd665d968582af962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ec167121708390077878e512bd52231",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6a94f8c9f1e3d1b70f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "20b1480931a4518b0eb8",
+    "url": "/static/js/2.d64f0eaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d64f0eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6a94f8c9f1e3d1b70f",
+    "url": "/static/js/main.c45c6ca6.chunk.js"
+  },
+  {
+    "revision": "37423abf38eba54c8b89",
+    "url": "/static/js/runtime-main.04cffe67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
